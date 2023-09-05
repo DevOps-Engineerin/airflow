12864ef0c8c7154651b13248384bf4a855e1e1e6
ganzamick <ganzamick@gmail.com>
2023-09-06 00:09:43 +0200

    adding a dag with a pythonoperator

--- Airflow_Docker/dags/my_second_dag.py
+++ Airflow_Docker/dags/my_second_dag.py
@@ -9,27 +9,31 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-def greet(age, ti):
+def greet(ti):
     first_name = ti.xcom_pull(task_ids='get_name', key='first_name')
     last_name = ti.xcom_pull(task_ids='get_name', key='last_name')
+    age = ti.xcom_pull(task_ids='get_age', key='age')
     print(f"hello world, My Name is {first_name} {last_name},"
           f"and I am {age} years old!")
 
 def get_name(ti):  
     ti.xcom_push(key='first_name', value='gmx')
     ti.xcom_push(key='last_name', value='the DJ')
+    
+def get_age(ti): 
+    ti.xcom_push(key='age', value=20)
 
 with DAG(
     default_args=default_args,
-    dag_id='ganza_dag_v11',
+    dag_id='ganza_dag_v12',
     description='this is my second dag that I write and the first with pythonoperator',
     start_date=datetime(2023, 9, 5),
     schedule_interval='@daily'
 ) as dag:
     task1 = PythonOperator(
         task_id='greet',
-        python_callable=greet,
-        op_kwargs={'age': 25}
+        python_callable=greet
+        #op_kwargs={'age': 25}
     )
     
     task2 = PythonOperator(
@@ -37,4 +41,9 @@ with DAG(
         python_callable=get_name
     )
     
-    task2 >> task1
+    task3 = PythonOperator(
+        task_id='get_age',
+        python_callable=get_age
+    )
+        
+    [task2, task3] >> task1
