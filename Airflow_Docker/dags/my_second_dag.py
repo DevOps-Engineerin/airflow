0cbaaf452000585b57da6d5db07d247c58d32f2b
ganzamick <ganzamick@gmail.com>
2023-09-05 15:10:05 +0200

    adding second dag with a pythonoperator

---
+++ Airflow_Docker/dags/my_second_dag.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+
+default_args = {
+    'owner': 'ganza_m',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}
+
+def greet():
+    print("hello world from pythonoperator")
+    
+
+with DAG(
+    default_args=default_args,
+    dag_id='ganza_dag_v6',
+    description='this is my second dag that I write and the first with pythonoperator',
+    start_date=datetime(2023, 9, 5),
+    schedule_interval='@daily'
+) as dag:
+    pass
