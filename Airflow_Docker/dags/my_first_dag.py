937da149dc75071537f5ff4ed599087d8221a4a3
ganzamick <ganzamick@gmail.com>
2023-09-05 13:29:47 +0200

    chore: adding 3 task

--- Airflow_Docker/dags/my_first_dag.py
+++ Airflow_Docker/dags/my_first_dag.py
@@ -3,13 +3,13 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 
 default_args ={
-    'owner': 'ganza',
+    'owner': 'ganza_m',
     'retries': 5,
     'retry_delay':timedelta(minutes=2)
 }
 
 with DAG(
-    dag_id='my_first_dag_v2',
+    dag_id='ganza_dag_v3',
     default_args=default_args,
     description='this is my first dag that we write',
     start_date=datetime(2023, 8, 30),
@@ -25,4 +25,10 @@ with DAG(
         bash_command="echo hello, this is a second task2 and it will run after task1 has runned"
     )
     
+    task3 = BashOperator(
+        task_id = 'third_task',
+        bash_command="echo hello, task3 here runnig after task2"
+    )
+    
     task1.set_downstream(task2)
+    task1.set_downstream(task3)
