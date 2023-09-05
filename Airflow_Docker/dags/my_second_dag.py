c3939a92ee3a54427ae08c8785bf0067472a10ae
ganzamick <ganzamick@gmail.com>
2023-09-05 16:19:17 +0200

    adding a dag with a pythonoperator

--- Airflow_Docker/dags/my_second_dag.py
+++ Airflow_Docker/dags/my_second_dag.py
@@ -9,17 +9,19 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-def greet(name, ti):
-    name = ti.xcom_pull(task_ids='get_name')
-    print(f"hello world from pythonoperator, My Name is {name},"
+def greet(age, ti):
+    first_name = ti.xcom_pull(task_ids='get_name', key='first_name')
+    last_name = ti.xcom_pull(task_ids='get_name', key='last_name')
+    print(f"hello world, My Name is {first_name} {last_name},"
           f"and I am {age} years old!")
 
-def get_name():  
-    return 'ganza' 
+def get_name(ti):  
+    ti.xcom_push(key='first_name', value='gmx')
+    ti.xcom_push(key='last_name', value='the DJ')
 
 with DAG(
     default_args=default_args,
-    dag_id='ganza_dag_v9',
+    dag_id='ganza_dag_v11',
     description='this is my second dag that I write and the first with pythonoperator',
     start_date=datetime(2023, 9, 5),
     schedule_interval='@daily'
