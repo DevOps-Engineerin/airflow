c4eeef29eda221e84d488de41eb8c27137839897
ganzamick <ganzamick@gmail.com>
2023-09-06 11:30:59 +0200

    chore: adding dag v16

--- Airflow_Docker/dags/dag_with_taskflow_api.py
+++ Airflow_Docker/dags/dag_with_taskflow_api.py
@@ -9,29 +9,34 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-@dag(dag_id='ganza_dag_v14',
+@dag(dag_id='ganza_dag_v16',
      default_args=default_args,
-     start_date=datetime(2023, 9, 5),
+     start_date=datetime(2023, 9, 6),
      schedule_interval='@daily',
 )
 
 def hello_world_etl():
     
-    @task()
+    @task(multiple_outputs=True)
     def get_name():
-        return 'gmx'
+        return {
+                'first_name': 'gmx',
+                'last_name': 'the DJ'
+                }
     
     @task()
     def get_age():
         return 20
     
     @task()
-    def greet(name, age):
-        print(f"hello world, My Name is {name},"
+    def greet(first_name, last_name, age):
+        print(f"hello world, My Name is {first_name} {last_name},"
               f"and I am {age} years old!") 
         
+    name_dict = get_name()
+    #last_name = get_name()
     name = get_name()
     age = get_age()
-    greet(name=name, age=age) 
+    greet(first_name=name_dict['first_name'], last_name=name_dict['last_name'], age=age) 
     
 greet_dag = hello_world_etl()
